3cca6d899a5a90df3bf4f64684bd15cb496e298a
jordantdale98 <jordantdale98@gmail.com>
2024-01-16 05:57:57 -0500

    changes made during lab

--- CD220Labs/http_server/index-with-require.js
+++ CD220Labs/http_server/index-with-require.js
@@ -3,7 +3,23 @@ const today = require('./today');
 
 const requestListener = function (req, res) {
   res.writeHead(200);
-  res.end(`Hello, World! The date today is ${today.getDate()}`);
+  //res.end(`Hello, World! The date today is ${today.getDate()}`);
+  let dateVal = today.getDate();
+  let dateTime = dateVal.getHours();
+  let greeting = "Hello World";
+  if (dateTime < 12){
+    greeting = "Good Morning!";
+  }
+  if (dateTime >= 12 && dateTime <= 17){
+    greeting = "Good Afternoon!";
+  }
+  if (dateTime > 17 && dateTime <=21 ){
+    greeting = "Good Evening!";
+  }
+  if (dateTime > 21){
+    greeting = "Good Night!";
+  }
+  res.end(`The date and time is ${dateVal}, ${greeting}`);
 }
 
 const port = 8080;
